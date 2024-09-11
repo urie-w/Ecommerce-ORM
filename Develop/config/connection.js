86e88f8a45d5284122d4a408e710cff601679906
urie-w <urieasia@gmail.com>
2024-09-11 10:06:48 -0600

    category-routes.js started

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,16 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 3001,
+    }
+);
 
 module.exports = sequelize;
